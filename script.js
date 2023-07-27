5d538cb4ce1d558d1659d056bf07d7fe1517436b
JONASANTOS <jonasantos16037@gmail.com>
2023-07-27 09:23:41 -0300

    portfólio

--- script.js
+++ script.js
@@ -1,79 +1,20 @@
-for (let numero = 0; numero < 100; numero++) {
-    console.log(`repetiçao de numero ${numero}`)
-}
 
-//Slider de projetos
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.projetos1');
-const prevBtn = document.querySelector('#btn-back');
-const nextBtn = document.querySelector('#btn-next');
-let slideIndex = 0;
 
-function showSlide(n) {
-  slides.forEach(slide => {
-    slide.style.transform = `translateX(-${n * 100}%)`;
-    slide.style.transition = 'transform 1s ease-in-out';
+//responsivo
+const Menu = document.querySelector('.menu')
+const Bar = document.querySelector('.nav-menu')
+
+
+Menu.addEventListener('click', () => {
+  Menu.classList.toggle('ativo')
+  Bar.classList.toggle('ativo')
+})
+
+
+document.querySelectorAll('a').forEach(a => {
+  a.addEventListener('click', () => {
+    Menu.classList.remove('ativo');
+    Bar.classList.remove('ativo');
   });
-}
+});
 
-function nextSlide() {
-  slideIndex++;
-  if (slideIndex > slides.length - 1) {
-    slideIndex = 0;
-  }
-  showSlide(slideIndex);
-}
-
-function prevSlide() {
-  slideIndex--;
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  }
-  showSlide(slideIndex);
-}
-
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', prevSlide);
-//FIM slider projetos
-
-const canvas = document.getElementById('Matrix');
-const context = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const nums = '0123456789';
-
-const alphabet = katakana + latin + nums;
-
-const fontSize = 16;
-const columns = canvas.width/fontSize;
-
-const rainDrops = [];
-
-for( let x = 0; x < columns; x++ ) {
-	rainDrops[x] = 1;
-}
-
-const draw = () => {
-	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	
-	context.fillStyle = '#0f0';
-	context.font = fontSize + 'px monospace';
-
-	for(let i = 0; i < rainDrops.length; i++)
-	{
-		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
-		
-		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
-			rainDrops[i] = 0;
-        }
-		rainDrops[i]++;
-	}
-};
-
-setInterval(draw, 30);
